236656a779c28b738ae7b66bc7f456ab0e0a51fc
Julian Marin <julian.marinc2@gmail.com>
2022-03-09 14:51:17 -0500

    antes de comenzar formularios

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,5 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("hello word")
+    lastest_question_list = Question.objects.all()
+    return render(request,"polls/index.html",{
+        "lastest_question_list" : lastest_question_list
+    })
+
+
+
+def detail(request, question_id):
+    question=get_object_or_404(Question, pk=question_id)
+    return render(request,"polls/detail.html",{
+        "question":question
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f"results{question_id}")
+
+
+def votes(request, question_id):
+    return HttpResponse(f"votes{question_id}")
